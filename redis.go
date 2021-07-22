50bc16b85ba69081ae992a85cb91f89e41511465
cc <87791508+ccgolib@users.noreply.github.com>
2021-07-22 17:29:42 +0800

    Update redis.go

--- redis.go
+++ redis.go
@@ -18,10 +18,14 @@ func main() {
 
 	t := time.Now()
 
-	rdb.Set(ctx, "cc1", 1, time.Second*100)
-	cc := rdb.Del(ctx, "cc1")
+	// lua
+	//redislua(rdb)
+
+	cc := rdb.Set(ctx, "cc", "cc", time.Second*100)
+	cc1 := rdb.Del(ctx, "cc1")
 	cc2 := rdb.Del(ctx, "cc2")
-	fmt.Println(cc, cc2)
+	fmt.Println(cc, cc1, cc2)
+
 	//for i:=0; i< 100; i++ {
 	//	fmt.Println(i)
 	//}
@@ -58,4 +62,32 @@ func main() {
 	fmt.Println(rdb.Get(ctx, "b"))
 	fmt.Println(rdb.Get(ctx, "c"))*/
 
+}
+
+/**
+http://www.redis.cn/commands/eval.html
+redis执行lua脚本
+KEYS[i] 全局变量，对应Eval keys参数，i从1开始
+ARGV[1] 局部变量，对应Eval args多参数，从1开始
+redis.call()
+tonumber() 数据类型转换
+*/
+func redislua(rdb *redis.Client) {
+	arrStr := []string{"cc", "cc1"}
+	RandomPushScript := `
+	local res
+	local i = tonumber(ARGV[1])
+	while(i>0) do
+		redis.call('SET',KEYS[i],KEYS[i])
+		i=i-1
+	end
+	res = redis.call('SET',ARGV[2],222)
+	return res
+	`
+	res := rdb.Eval(ctx, RandomPushScript, arrStr, len(arrStr), "cc2")
+
+	fmt.Println(res.Val())
+	fmt.Println(rdb.Get(ctx, "cc"))
+	fmt.Println(rdb.Get(ctx, "cc1"))
+	fmt.Println(rdb.Get(ctx, "cc2"))
 }
